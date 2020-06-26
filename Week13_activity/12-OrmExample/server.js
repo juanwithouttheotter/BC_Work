a7bbc18c13ab7d9c52f6fff090e1b87c1f9fa47b
Juan Osorio <juan.michael.osorio@gmail.com>
2020-06-26 16:18:13 -0500

    notes to the ORM example

--- Week13_activity/12-OrmExample/server.js
+++ Week13_activity/12-OrmExample/server.js
@@ -2,6 +2,7 @@ var orm = require("./config/orm.js");
 
 // Find all the pets ordering by the lowest price to the highest price.
 orm.selectAndOrder("animal_name", "pets", "price");
+//orm.selectAndOrder({select: "animal_name", table: "pets", column: "price"});
 
 // Find a pet in the pets table by an animal_name of Rachel.
 orm.selectWhere("pets", "animal_name", "Rachel");
